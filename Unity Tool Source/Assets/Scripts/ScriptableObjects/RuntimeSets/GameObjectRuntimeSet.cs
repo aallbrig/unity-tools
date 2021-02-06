76cf49a51827c488033d7d2339c205ceb949ec3d
Andrew Allbright <aallbrig@gmail.com>
2021-02-06 14:14:36 -0500

    Game Object runtime set

---
+++ Unity Tool Source/Assets/Scripts/ScriptableObjects/RuntimeSets/GameObjectRuntimeSet.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.RuntimeSets
+{
+	[CreateAssetMenu(fileName = "new game object runtime set", menuName = "AATools/RuntimeSets/GameObject")]
+	public class GameObjectRuntimeSet : RuntimeSet<GameObject> {}
+}
---
+++ Unity Tool Source/Assets/Scripts/ScriptableObjects/RuntimeSets/RuntimeSet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableObjects.RuntimeSets
+{
+	public abstract class RuntimeSet<T> : ScriptableObject
+	{
+		public List<T> list = new List<T>();
+
+		public void Add(T controller)
+		{
+			if (!list.Contains(controller)) list.Add(controller);
+		}
+
+		public void Remove(T controller)
+		{
+			if (list.Contains(controller)) list.Remove(controller);
+		}
+
+		private void Awake() => list.Clear();
+		private void OnEnable() => list.Clear();
+		private void OnDisable() => list.Clear();
+		private void OnDestroy() => list.Clear();
+	}
+}
