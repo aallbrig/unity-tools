79cc56e6711a96019805e974e9eb6ea7d7cc23d1
Andrew Allbright <aallbrig@gmail.com>
2021-02-06 14:08:12 -0500

    Event and event listener created for game object

---
+++ Unity Tool Source/Assets/MonoBehaviours/EventListeners/GameObjectEventListener.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ScriptableObjects.Events;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MonoBehaviours.EventListeners
+{
+	[Serializable] public class GameObjectEventUnityEvent : UnityEvent<GameObject> {}
+
+	public class GameObjectEventListener : MonoBehaviour
+	{
+		public GameObjectEvent soEvent;
+		public GameObjectEventUnityEvent unityEvent;
+
+		public void OnEnable() => soEvent.RegisterListener(this);
+
+		public void OnDisable() => soEvent.UnregisterListener(this);
+
+		public void OnEventBroadcast(GameObject go) => unityEvent.Invoke(go);
+	}
+}
---
+++ Unity Tool Source/Assets/Scripts/ScriptableObjects/Events/GameObjectEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using MonoBehaviours.EventListeners;
+using UnityEngine;
+
+namespace ScriptableObjects.Events
+{
+	[CreateAssetMenu(fileName = "new game object event", menuName = "AATools/Events/GameObjectEvent")]
+	public class GameObjectEvent : ScriptableObject
+	{
+		private readonly List<GameObjectEventListener> _listeners = new List<GameObjectEventListener>();
+
+		public void Broadcast(GameObject go)
+		{
+			for (var i = _listeners.Count - 1; i >= 0; i--)
+				_listeners[i].OnEventBroadcast(go);
+		}
+
+		public void RegisterListener(GameObjectEventListener listener)
+		{
+			if (!_listeners.Contains(listener))
+				_listeners.Add(listener);
+		}
+
+		public void UnregisterListener(GameObjectEventListener listener)
+		{
+			if (_listeners.Contains(listener))
+				_listeners.Remove(listener);
+		}
+	}
+}
