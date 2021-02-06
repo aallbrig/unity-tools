7cff9ad23bb30778262793c516f7063395eacc2c
Andrew Allbright <aallbrig@gmail.com>
2021-02-06 14:24:25 -0500

    ColorVar and ColorRef are available

---
+++ Unity Tool Source/Assets/Scripts/ScriptableObjects/Vars/ColorVar.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Vars
+{
+	[CreateAssetMenu(fileName = "new color var", menuName = "AATools/Vars/ColorVar", order = 0)]
+	public class ColorVar : ScriptableObject
+	{
+		public Color value = Color.white;
+	}
+}
