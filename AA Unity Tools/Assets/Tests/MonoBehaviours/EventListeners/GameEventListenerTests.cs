4849a67ee46b5a29bd3e0ded3440561e2d85a2f7
Andrew Allbright <aallbrig@gmail.com>
2021-02-07 13:00:02 -0500

    GameEventListener MonoBehaviour unit test available

---
+++ AA Unity Tools/Assets/Tests/MonoBehaviours/EventListeners/GameEventListenerTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using MonoBehaviours.EventListeners;
+using NUnit.Framework;
+using ScriptableObjects.Events;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.TestTools;
+
+namespace Tests.MonoBehaviours.EventListeners
+{
+    public class GameEventListenerTests
+    {
+        private GameObject _gameObject;
+        private GameEventListener _gameEventListener;
+        private GameEvent _testGameEvent;
+        private UnityEvent _unityEvent;
+
+        [SetUp]
+        public void Setup()
+        {
+            _unityEvent = new UnityEvent();
+            _unityEvent.AddListener(() => Assert.Pass("Unity Event listener invoked"));
+
+            _testGameEvent = ScriptableObject.CreateInstance<GameEvent>();
+
+            _gameObject = new GameObject();
+            _gameEventListener = _gameObject.AddComponent<GameEventListener>();
+            _gameEventListener.soEvent = _testGameEvent;
+            _gameEventListener.unityEvent = _unityEvent;
+            _gameEventListener.OnEnable();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _gameEventListener.OnDisable();
+            _gameEventListener = null;
+            _gameObject = null;
+            _testGameEvent = null;
+        }
+
+        [Test]
+        public void GameEventListenerUnityEventCanBeInvokedManually()
+        {
+            _gameEventListener.OnEventBroadcast();
+        }
+
+        [Test]
+        public void GameEventListenerInvokesUnityEventOnGameEventBroadcast()
+        {
+            _testGameEvent.Broadcast();
+        }
+    }
+}
