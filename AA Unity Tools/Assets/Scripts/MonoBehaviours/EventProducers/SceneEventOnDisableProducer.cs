44156539fc1d74309d4e2510f7d24c90dffaaf56
Andrew Allbright <aallbrig@gmail.com>
2021-02-25 14:40:37 -0500

    Demo scene enter & leave events are produced by the demo scene

---
+++ AA Unity Tools/Assets/Scripts/MonoBehaviours/EventProducers/SceneEventOnDisableProducer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ScriptableObjects.Events;
+using UnityEngine;
+
+namespace MonoBehaviours.EventProducers
+{
+    public class SceneEventOnDisableProducer : MonoBehaviour
+    {
+        public List<SceneEvent> sceneEvents;
+
+        private void OnDisable() => sceneEvents.ForEach(sceneEvent => sceneEvent.Broadcast());
+    }
+}
---
+++ AA Unity Tools/Assets/Scripts/MonoBehaviours/EventProducers/SceneEventOnEnableProducer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ScriptableObjects.Events;
+using UnityEngine;
+
+namespace MonoBehaviours.EventProducers
+{
+    public class SceneEventOnEnableProducer : MonoBehaviour
+    {
+        public List<SceneEvent> sceneEvents;
+
+        private void OnEnable() => sceneEvents.ForEach(sceneEvent => sceneEvent.Broadcast());
+    }
+}
