717b123f1eaae7c61206e467a11d86ead362f464
Andrew Allbright <aallbrig@gmail.com>
2021-02-25 15:33:24 -0500

    Player sees more content on the credits and instruction menu screens

--- AA Unity Tools/Assets/Scripts/MonoBehaviours/Customizers/TextMeshProTextCustomizer.cs
+++ AA Unity Tools/Assets/Scripts/MonoBehaviours/Customizers/TextMeshProTextCustomizer.cs
@@ -9,7 +9,7 @@ namespace MonoBehaviours.Customizers
     {
         public StringRef text;
         public ColorRef fontColor;
-        public IntRef fontSize;
+        public IntRef fontSize = new IntRef{var = 36};
         private TextMeshProUGUI _text;
 
         private void Start() => _text = GetComponent<TextMeshProUGUI>();
