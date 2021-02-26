32c4104effaaab73b40ebcec19147dfa3ba91963
Andrew Allbright <aallbrig@gmail.com>
2021-02-25 23:03:37 -0500

    Model an animation timeline

---
+++ AA Unity Tools/Assets/Scripts/MonoBehaviours/EventProducers/LogOnAnimationEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace MonoBehaviours.EventProducers
+{
+    public class LogOnAnimationEvent : MonoBehaviour
+    {
+        public void OnAnimationEvent() => Debug.Log("[LogOnAnimationEvent] Animation event has fired");
+    }
+}
