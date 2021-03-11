0a4f0699e51df5f96ca3dd58a57778d4d4f334af
Andrew Allbright <aallbrig@gmail.com>
2021-03-11 08:43:31 -0500

    Constant rotation added

---
+++ AA Unity Tools/Packages/com.andrewallbright.allbrighttools/Runtime/MonoBehaviours/Customizers/ConstantRotation.cs
@@ -0,0 +1,14 @@
+﻿using ScriptableObjects.Refs;
+using UnityEngine;
+
+namespace MonoBehaviours.Customizers
+{
+    public class ConstantRotation : MonoBehaviour
+    {
+        public FloatRef rotationSpeed = new FloatRef {var = 10.0f};
+        private Transform _transform;
+        private void Update() => _transform.Rotate(0, rotationSpeed.Value * Time.deltaTime, 0);
+
+        private void OnEnable() => _transform = transform;
+    }
+}
